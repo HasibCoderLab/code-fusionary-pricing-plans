3ad611d84eafea08b39757a09af91c101a9a98b8
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-10-03 21:33:47 +0600

    fixed js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // Toggle show/hide plan options
 document.querySelector(".toggle-btn").addEventListener("click", () => {
     const options = document.getElementById("plan-options-std");
-    options.style.display = options.style.display = "block" ? "none" : block
+    options.style.display = options.style.display = "block" ? "none" : block;
+    // console.log(options, "Dome");
+    
 });
 const setPlan = (plan, type) => {
     if (paln === "std") {
@@ -9,6 +11,10 @@ const setPlan = (plan, type) => {
             document.getElementById("price-std").innerText = "$19";
             document.getElementById("duration-std").innerText = "/per month";
 
+        } else if (type === "year") {
+            document.getElementById("price-std").innerText = "$190";
+            document.getElementById("duration-std").innerText = "/per year";
         }
     }
+    document.getElementById("plan-options-std").style.display = "none";
 }
